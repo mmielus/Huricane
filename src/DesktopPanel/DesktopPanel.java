af180fa8de18de77b9c65b634ad56f4cfe014e27
Michal <michal.mielus@gmail.com>
2017-12-18 19:15:18 +0100

    Dodany interfejs do kalibracji

--- src/DesktopPanel/DesktopPanel.java
+++ src/DesktopPanel/DesktopPanel.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.EventQueue;
 import java.awt.image.BufferStrategy;
 import java.util.ArrayList;
@@ -15,81 +16,114 @@ import java.util.Random;
 
 
 public class DesktopPanel extends JFrame {
-  //  public ArrayList<Tree> forest = new ArrayList<>(500);
     public Tree forest[][] = new Tree[800][600];
     long l = System.currentTimeMillis();
     private BufferStrategy bufferstrat = null;
     private Canvas render;
-    private int lit=1;
-    private int mnoz=1;
-    public DesktopPanel(){
+    private int lit = 1;
+    private int mnoz = 1;
+    public ButtonTest test = new ButtonTest();
+
+
+    public DesktopPanel() {
+
         super();
         setTitle("Hurricane");
         setIgnoreRepaint(true);
         setResizable(false);
-
         render = new Canvas();
         render.setIgnoreRepaint(true);
-        setBounds(50,50,800,600);
-        render.setBounds(50,50,800,600);
-
-        add(render);
+        setLayout(new BorderLayout());
+        setBounds(50, 50, 800, 600);
+        render.setBounds(50, 50, 800, 600);
+        JPanel panel = new ButtonPanel();
+        panel.setIgnoreRepaint(true);
+        panel.setBounds(700, 100, 100, 100);
+        add(render, BorderLayout.WEST);
         pack();
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         render.createBufferStrategy(2);
         bufferstrat = render.getBufferStrategy();
     }
-    public void addTree(int x,int y){
+
+    /**
+     * Metoda dodajaca drzewo do lasu
+     *
+     * @param x wspolrzedna
+     * @param y wspolrzedna
+     */
+    public void addTree(int x, int y) {
         forest[x][y] = (new Tree(x, y, 5, 5, Color.GREEN));
     }
-    public void renderForest(Graphics2D g2d){
-        Tree tree=null;
-        for(int i=0;i<800;i++)
-            for(int j=0;j<600;j++)
-                if(forest[i][j]!=null) {
+
+    /**
+     * Wyswietla las renderujac kazde drzewo po kolei.
+     *
+     * @param g2d
+     */
+    public void renderForest(Graphics2D g2d) {
+        Tree tree = null;
+        for (int i = 0; i < 800; i++)
+            for (int j = 0; j < 600; j++)
+                if (forest[i][j] != null) {
                     tree = forest[i][j];
                     tree.render(g2d);
                 }
-        
+
 
     }
+
+    /** Metoda tworzaca las
+     *
+     */
     public void makeForest() {
         Random generator = new Random();
-        int l=0;
-        while(l<20000) {
-            addTree(generator.nextInt(800),generator.nextInt(600) );
-         l++;
+        int l = 0;
+        while (test.getCheck() == false) {
+            System.out.println(test.forestDensity);
         }
-
-
+        while (l < test.getForestDensity()) {
+            addTree(generator.nextInt(800), generator.nextInt(600));
+            l++;
+        }
     }
-    public void render(){
-        do{
-            do{
+
+    /**
+     *
+     */
+    public void render() {
+        do {
+            do {
                 Graphics2D g2d = (Graphics2D) bufferstrat.getDrawGraphics();
                 g2d.fillRect(0, 0, render.getWidth(), render.getHeight());
 
                 renderForest(g2d);
 
                 g2d.dispose();
-            }while(bufferstrat.contentsRestored());
+            } while (bufferstrat.contentsRestored());
             bufferstrat.show();
-        }while(bufferstrat.contentsLost());
+        } while (bufferstrat.contentsLost());
     }
-    public void loop(){
+
+    /**
+     *
+     */
+    public void loop() {
         Random generator = new Random();
-        Tree tree=null;
+        Tree tree = null;
         makeForest();
-        while(true){
+        while (true) {
 
-
-            if(l+500<System.currentTimeMillis()) {
-                l=System.currentTimeMillis();
+/**
+ * ta czesc ponizej jest od tego zeby zmienialo zielone kropki na czerwone
+ * o ile sie nie myle to w tej czesci bedzie trzeba polaczyc to co napisal epy zeby jakos to symulowalo odpowiednio
+ */
+            if (l + 500 < System.currentTimeMillis()) {
+                l = System.currentTimeMillis();
                 mnoz++;
-                int a = generator.nextInt(10)*mnoz;
-                for(int i=0;i<580;i++) {
+                int a = generator.nextInt(10) * mnoz;
+                for (int i = 0; i < 580; i++) {
                     if (forest[a][i + 2] != null) {
                         tree = forest[a][i + 2];
                         tree.changeColor(Color.red);
@@ -100,11 +134,10 @@ public class DesktopPanel extends JFrame {
             render();
 
             try {
-                Thread.sleep(1000/30);
+                Thread.sleep(1000 / 30);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 }
-
--- src/DesktopPanel/Test.java
+++ src/DesktopPanel/Test.java
@@ -4,14 +4,13 @@ import java.awt.EventQueue;
 
 public class Test {
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
+       {
 
                 DesktopPanel panel =new DesktopPanel();
+
                 panel.loop();
             }
-        });
+
     }
 }
 
